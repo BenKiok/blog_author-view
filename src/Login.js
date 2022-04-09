8f318ceb2909c5d1f8a22b039bd52ead64548d03
Benedict Kioko <benedictmkioko@gmail.com>
2022-04-09 17:54:56 -0400

    Create form function that retrieves JSON web token from backend authentication

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,28 @@
 function Login() {
+  const fetchLoginToken = async (event, func) => {
+    event.preventDefault();
+    const loginData = JSON.stringify({
+      username: event.target[0].value,
+      password: event.target[1].value
+    });
+    const token = await fetch('http://localhost:4000/api/user/login', 
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: loginData
+    })
+    .then(res => res.json())
+    .catch(err => console.log(err));
+
+    func(token);
+  }
+
   return (
     <div className='login'>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={e => fetchLoginToken(e, d => console.log(d))}>
         <label>Username
           <input type='text' name='username' placeholder='JohnDoe123'/>
         </label>
